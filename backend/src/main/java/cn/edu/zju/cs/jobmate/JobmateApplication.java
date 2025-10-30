604edb63fb10fb898fd455ea13d21e4024231625
1EEExp3rt <1EEExpErt@zju.edu.cn>
2025-10-31 00:35:40 +0800

    chore: 添加后端基础框架

---
+++ backend/src/main/java/cn/edu/zju/cs/jobmate/JobmateApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.zju.cs.jobmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobmateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cn/edu/zju/cs/jobmate/JobmateApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zju.cs.jobmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
