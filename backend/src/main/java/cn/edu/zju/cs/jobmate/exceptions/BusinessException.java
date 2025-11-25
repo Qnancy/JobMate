f3fb93d2d12ce735676a37637ffe5c45e956eec2
1EEExp3rt <1EEExpErt@zju.edu.cn>
2025-11-26 01:16:14 +0800

    feat: 业务异常支持添加原始异常以辅助问题排查与异常追溯

--- backend/src/main/java/cn/edu/zju/cs/jobmate/exceptions/BusinessException.java
+++ backend/src/main/java/cn/edu/zju/cs/jobmate/exceptions/BusinessException.java
@@ -9,7 +9,24 @@ public class BusinessException extends RuntimeException {
 
     private final ErrorCode errorCode;
 
+    /**
+     * Constructor.
+     * 
+     * @param errorCode ErrorCode enum.
+     */
     public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    /**
+     * Constructor with cause.
+     * 
+     * @param errorCode ErrorCode enum.
+     * @param cause     The original exception cause.
+     */
+    public BusinessException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.getMessage(), cause);
         this.errorCode = errorCode;
     }
 
