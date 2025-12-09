dd4a4c4f14b2f51ac0875bf5577c83fc66f35175
1EEExp3rt <1EEExpErt@zju.edu.cn>
2025-12-09 21:09:00 +0800

    test: 更新 WebMvcConfig 测试

---
+++ backend/src/test/java/cn/edu/zju/cs/jobmate/configs/WebMvcConfigTest.java
@@ -0,0 +1,52 @@
+package cn.edu.zju.cs.jobmate.configs;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import cn.edu.zju.cs.jobmate.configs.properties.MonitorProperties;
+import cn.edu.zju.cs.jobmate.utils.DummyControllerUtil;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = DummyControllerUtil.class)
+@AutoConfigureMockMvc(addFilters = false)
+@Import(value = MonitorProperties.class)
+class WebMvcConfigTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private StringRedisTemplate stringRedisTemplate;
+
+    long cnt; // Mock rate limit counter.
+
+    @BeforeEach
+    @SuppressWarnings({"null", "unchecked"})
+    void setUp() {
+        cnt = 0;
+        ValueOperations<String, String> valueOps = Mockito.mock(ValueOperations.class);
+        Mockito.when(stringRedisTemplate.opsForValue()).thenReturn(valueOps);
+        Mockito.when(valueOps.increment(anyString())).thenAnswer(invocation -> ++cnt);
+    }
+
+    @Test
+    void testRateLimitInterceptor() throws Exception {
+        for (int i = 0; i < 5; i++) {
+            mockMvc.perform(get("/api/test")).andExpect(status().isOk());
+        }
+        mockMvc.perform(get("/api/test")).andExpect(status().isTooManyRequests());
+    }
+}
